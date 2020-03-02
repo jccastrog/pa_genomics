e4553f12fd37eb5dbfe9a2b0aebda6296b6bb4c5
jccastrog <jccastrogo@gmail.com>
2020-03-02 15:55:46 -0500

    Debugged for cd-hit clustr formated data, added output directory option

--- parseExtracyOGs.py
+++ parseExtracyOGs.py
@@ -2,7 +2,7 @@
 '''
 @name: parseExtracyOGs.py
 @author: Juan C. Castro <jccastrog at gatech dot edu>
-@update: 05-Jan-2018
+@update: 02-Mar-2020
 @version: 1.0
 @license: GNU General Public License v3.0.
 please type "./parseExtracyOGs.py -h" for usage help
@@ -24,6 +24,7 @@ def argParser():
 	group = parser.add_argument_group('Required arguments')
 	group.add_argument('-f', action='store', dest='ogs_file', required=True, help='A file with the ogs for the genomes in the pangneome.')
 	group.add_argument('-d', action='store', dest='genome_dir', required=True, help='A directory including the fasta files for the corresponding genomes.')
+	group.add_argument('-o', action='store', dest='out_dir', required=True, help='Directory where the output file will be stored.')
 	args = parser.parse_args()
 	return(args)
 def getOGdict(ogs_file):
@@ -40,20 +41,21 @@ def getOGdict(ogs_file):
 				k += 1
 			else :
 				fields = line.split('\t')
-				og = 'OG_{}'.format(k)
+				#og = 'OG_{}'.format(k) # For miga formating
+				og = fields[0] # For cd-hit clustr formating
 				og_dict[og] = dict()
-				for i in xrange(0,num_genomes):
+				for i in xrange(1,num_genomes):
 					genome = genome_ids[i]
-					if fields[i] == '-':
-						continue
-					else :
+					if fields[i] != '-':
 						gene = fields[i].split(',')[0]
+						#print(genome+"\t"+gene)
 						og_dict[og][genome] = gene
+						#print(og_dict[og])
 				k += 1
 	return(k, og_dict)
-def  extractOGs(og_dict, fasta_dir):
+def  extractOGs(og_dict, fasta_dir, out_dir):
 	for og in og_dict:
-		fasta_name = '{}.fasta'.format(og)
+		fasta_name = '{}/{}.fasta'.format(out_dir,og)
 		out_file = open(fasta_name, 'wb')
 		current_dict = og_dict[og]
 		for genome in current_dict:
@@ -68,61 +70,10 @@ def  extractOGs(og_dict, fasta_dir):
 						out_file.write(id_name)
 						out_file.write(str(seq)+'\n')
 		out_file.close()
-def getGeneList(ogs_file):
-	gene_dict = dict()
-	with open(ogs_file) as ogs:
-		lines = ogs.readlines()
-		k = 0
-		for line in lines:
-			line = line.rstrip('\n')
-			if k == 0:
-
-				genome_list = line.split('\t')
-				k += 1
-			else :
-				fields = line.split('\t')
-				i = 0
-				for field in fields:
-					if len(field.split(',')) > 1:
-						i += 1
-						continue
-					else:
-						if field == '-':
-							i += 1
-							continue
-						else:
-							if genome_list[i] in gene_dict:
-								gene_dict[genome_list[i]].append(field)
-							else:
-								gene_dict[genome_list[i]] = [field]
-							i += 1
-							break
-				k += 1
-	return(gene_dict)	
-def createPanFasta(fasta_dir, gene_dict, fasta_out):
-	num_genes = len(gene_dict)
-	out_file = open(fasta_out, 'wb')
-	with progressbar.ProgressBar(max_value=num_genes) as bar:
-		i = 1
-		for file in os.listdir(fasta_dir):
-			if file.endswith('.fna') or file.endswith('fa'):
-				genome_name = os.path.splitext(file)[0]
-				if genome_name in gene_dict:
-					with open(file) as fasta_file:
-						print len(SeqIO.parse(fasta_file, "fasta"))
-						for fasta_parse in SeqIO.parse(fasta_file, "fasta"):
-							ID = fasta_parse.id
-							seq = fasta_parse.seq
-							if ID in gene_dict[genome_name]:
-								out_file.write('>{}|{}\n'.format(genome_name,ID))
-								out_file.write('{}\n'.format(seq))
-								bar.update(i)
-								i += 1
-	out_file.close()
 
 '''================== 2.0 Download the files =================='''
 if __name__=="__main__":
 	args = argParser()
 	num_ogs,ogs_dict = getOGdict(args.ogs_file)
-	extractOGs(ogs_dict, args.genome_dir)
+	extractOGs(ogs_dict, args.genome_dir, args.out_dir)
 '''============================================================'''
